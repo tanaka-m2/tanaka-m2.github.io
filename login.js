b8fb85f03d6a9f4261a1bdebf5b4d180fb3b0308
tanakam2 <tanaka-m@sendapp.uk>
2024-09-15 12:18:04 -0400

    first

---
+++ login.js
@@ -0,0 +1,17 @@
+function validateForm() {
+  var userid = document.getElementById("userid").value;
+  var password = document.getElementById("password").value;
+  
+  if (userid === "512" && password === "tanaka") {
+    window.location.href = "id/1.html";
+    return false;
+  } 
+  else if (userid === "1" && password === "panchan") {
+    window.location.href = "id/62712566/1.html";
+    return false;
+  }
+  else {
+    alert("お前部外者だな?不正アクセス禁止法違反だぞ!");
+    return false;
+  }
+}
