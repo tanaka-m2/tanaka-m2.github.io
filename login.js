52e0525cbd7482e65fe333bc5286137ec119b608
tanakam2 <tanaka-m@sendapp.uk>
2024-09-16 22:37:58 -0400

    a

--- login.js
+++ login.js
@@ -2,24 +2,40 @@ function validateForm() {
   var userid = document.getElementById("userid").value;
   var password = document.getElementById("password").value;
   
-  if (userid === "512" && password === "tanaka") {
-    window.location.href = "id/1.html";
+  if (userid === "-1" && password === "admin") {
+    window.location.href = "id/admin.html";
     return false;
   } 
-  else if (userid === "1" && password === "panchan") {
-    window.location.href = "id/62712566/1.html";
+  else if (userid === "mako" && password === "0000") {
+    window.location.href = "id/member/25512087/mako.html";
     return false;
+  } 
+  else if (userid === "1" || userid === "Panda") {
+    if (password === "panchan") {
+      window.location.href = "id/62712566/1.html";
+      return false;
+    }
+    else {
+      alert("パスワードが違います。お忘れの場合はサイト下部のメールアドレスにお問い合わせください。");
+      return false;
+    }
   }
-  else if (userid === "2" && password === "1209") {
-    window.location.href = "id/33599693/2.html";
-    return false;
+  else if (userid === "2" || userid === "yukaminn") {
+    if (password === "1209") {
+      window.location.href = "id/33599693/2.html";
+      return false;
+    }
+    else {
+      alert("パスワードが違います。お忘れの場合はサイト下部のメールアドレスにお問い合わせください。");
+      return false;
+    }
   }
   else if (userid === "" && password === "") {
-    alert("番号とパスワードを入力してください");
+    alert("番号またはユーザー名とパスワードを入力してください");
     return false;
   }
   else {
-    alert("お前部外者だな?不正アクセス禁止法違反だぞ!");
+    alert("存在しないIDまたはユーザー名です");
     return false;
   }
 }
